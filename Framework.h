896239f8dbb86e2c518c7a81bb0ae27196f3d9ec
Trevor Cook <cook.trevor@outlook.com>
2022-03-05 11:26:21 -0500

    Add project files.

---
+++ Framework.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <glad/glad.h> // GLAD for retrieving hardware device drivers for OpenGL
+#include <GLFW/glfw3.h> // GLFW for creating and managing a window
---
+++ Main.cpp
@@ -0,0 +1,8 @@
+#include "Framework.h"
+
+int main()
+{
+
+	return 0;
+}
+
